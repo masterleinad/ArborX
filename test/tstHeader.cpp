c14c65738262ce341d1a5367a175b3001189809c
Damien L-G <dalg24@gmail.com>
2019-05-02 16:05:14 -0400

    Fix the CMake globbing expression to get all the headers

--- test/tstHeader.cpp
+++ test/tstHeader.cpp
@@ -1,9 +1,5 @@
 // include header twice to see if the include guards are set correctly
-#include @HEADER@
-#include @HEADER@
+#include @HEADER @
+#include @HEADER @
 
-int
-main()
-{
-  return 0;
-}
+int main() { return 0; }
