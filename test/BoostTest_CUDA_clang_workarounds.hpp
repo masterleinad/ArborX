d5f76aca1c5857987241e4cf7468c8227a711ee3
Daniel Arndt <arndtd@ornl.gov>
2019-08-29 21:22:39 +0000

    Address comments

--- test/BoostTest_CUDA_clang_workarounds.hpp
+++ test/BoostTest_CUDA_clang_workarounds.hpp
@@ -1,6 +1,8 @@
 #ifndef BOOSTTEST_CUDA_CLANG_WORKAROUNDS_HPP
 #define BOOSTTEST_CUDA_CLANG_WORKAROUNDS_HPP
 
+#include <boost/fusion/adapted/std_tuple.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
 #include <boost/type_traits/add_const.hpp>
 #include <boost/type_traits/add_pointer.hpp>
 
